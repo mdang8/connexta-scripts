92ad9587efad05822448f6161e88bd893966dfeb
mdang8 <mdang808@gmail.com>
2018-10-24 16:25:26 -0700

    Wait for CDM directory to be empty before copying over files

--- copy_metacards.sh
+++ copy_metacards.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-METACARD_DIRECTORY=~/Downloads/xml
-CDM_DIRECTORY=/tmp/cdm
+METACARD_DIR=~/Downloads/xml
+CDM_DIR=/tmp/cdm
 
 START_TIME=$SECONDS
 
 for i in `seq 1 25`;
 do
-    cp ${METACARD_DIRECTORY}/*.xml $CDM_DIRECTORY
-    echo "Copied 5 XML files to ${CDM_DIRECTORY}"
+    # counts the # of lines in the ls output for the CDM directory
+    NUM_FILES_IN_CDM_DIR=$(( ls -l CDM_DIR | wc -l ))
+    # sleep for 1 seconds and check for empty directory
+    while [ $NUM_FILES_IN_CDM_DIR -gt 0 ]
+    do
+        sleep 1
+        NUM_FILES_IN_CDM_DIR=$(( ls -l CDM_DIR | wc -l ))
+    done
+
+    # copies the XML files and sleeps for 5 seconds to wait for the metacards to be ingested and moved
+    cp ${METACARD_DIR}/*.xml $CDM_DIR
+    echo "Copied 5 XML files to ${CDM_DIR}"
     sleep 5
 done
 
 END_TIME=$SECONDS
 
-echo "\nFinished copying $(( 5 * 25 )) files in $(( END_TIME - START_TIME  )) seconds."
+echo "Finished copying $(( 5 * 25 )) files in $(( END_TIME - START_TIME  )) seconds."
 
