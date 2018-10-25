216df40fd6a7717af599593ac6add6237bf94df3
mdang8 <mdang808@gmail.com>
2018-10-25 09:18:56 -0700

    Fixed file count expression evaluation

--- copy_metacards.sh
+++ copy_metacards.sh
@@ -8,12 +8,12 @@ START_TIME=$SECONDS
 for i in `seq 1 25`;
 do
     # counts the # of lines in the ls output for the CDM directory
-    NUM_FILES_IN_CDM_DIR=$(( ls -l CDM_DIR | wc -l ))
+    NUM_FILES_IN_CDM_DIR=$( ls -l $CDM_DIR | wc -l )
     # sleep for 1 seconds and check for empty directory
     while [ $NUM_FILES_IN_CDM_DIR -gt 0 ]
     do
         sleep 1
-        NUM_FILES_IN_CDM_DIR=$(( ls -l CDM_DIR | wc -l ))
+        NUM_FILES_IN_CDM_DIR=$( ls -l $CDM_DIR | wc -l )
     done
 
     # copies the XML files and sleeps for 5 seconds to wait for the metacards to be ingested and moved
